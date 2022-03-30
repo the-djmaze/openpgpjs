cf7dba74588c8a71c9b4a2e9ef105a8c3f8a970f
djmaze <>
2022-03-30 23:49:10 +0200

    Bugfix: i broke OpenPGP.js v5.2.1

--- src/biginteger/index.js
+++ src/biginteger/index.js
@@ -4,9 +4,10 @@ import BigInteger from './native.interface';
 async function getBigInteger() {
   if (util.detectBigInt()) {
     return BigInteger;
+  } else {
+    const { default: BigInteger } = await import('./bn.interface');
+    return BigInteger;
   }
-  const { default: BigInteger } = await import('./bn.interface');
-  return BigInteger;
 }
 
 // eslint-disable-next-line import/prefer-default-export
